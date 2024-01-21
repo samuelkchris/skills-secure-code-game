88b09b7aacbfcccb98d8ae7d31bddbe0f3424f81
samuelkchris <samuelkchris@gmail.com>
2024-01-21 22:21:10 +0300

    updated the path and vulnabilities

--- Season-2/Level-3/code.py
+++ Season-2/Level-3/code.py
@@ -17,10 +17,12 @@
 import os
 import re
 from flask import Flask, request, render_template
+from markupsafe import escape
+
 app = Flask(__name__)
 
 # Set the absolute path to the template directory
-template_dir = os.path.abspath('Season-2/Level-3/templates')
+template_dir = os.path.abspath('C:\\Users\\samue\\PycharmProjects\\skills-secure-code-game\\Season-2\\Level-3\\templates')
 app.template_folder = template_dir
 
 # Hard-coded planet data
@@ -32,26 +34,26 @@ planet_data = {
     "Jupiter": "The largest planet in the Solar System and known for its great red spot.",
 }
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         planet = request.form.get('planet')
-        sanitized_planet = re.sub(r'[<>{}[\]]', '', planet if planet else '')
+        sanitized_planet = escape(planet) if planet else ''
 
         if sanitized_planet:
-            if 'script' in sanitized_planet.lower() :
-                return '<h2>Blocked</h2></p>'
-    
-            return render_template('details.html', 
-                                   planet=sanitized_planet, 
+            return render_template('details.html',
+                                   planet=sanitized_planet,
                                    info=get_planet_info(sanitized_planet))
         else:
             return '<h2>Please enter a planet name.</h2>'
 
     return render_template('index.html')
 
+
 def get_planet_info(planet):
     return planet_data.get(planet, 'Unknown planet.')
+
 
 if __name__ == '__main__':
     app.run()
